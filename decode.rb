62d0b6b0aff3562124952dbcaed576ab8d7f36c0
baqar-abbas <baqar007@hotmail.com>
2023-07-20 00:19:46 +0500

    Defined a Class MorseCodeDecode methods for decoding character, word and message has been implemented.

--- decode.rb
+++ decode.rb
@@ -1,22 +1,61 @@
-# Method to Decode a Morse Code Character:
-def decode_char(morse_code)
-  morse_to_char = {
-    '.-'   => 'A',   '-...' => 'B',   '-.-.' => 'C',   '-..'  => 'D',
-    '.'    => 'E',   '..-.' => 'F',   '--.'  => 'G',   '....' => 'H',
-    '..'   => 'I',   '.---' => 'J',   '-.-'  => 'K',   '.-..' => 'L',
-    '--'   => 'M',   '-.'   => 'N',   '---'  => 'O',   '.--.' => 'P',
-    '--.-' => 'Q',   '.-.'  => 'R',   '...'  => 'S',   '-'    => 'T',
-    '..-'  => 'U',   '...-' => 'V',   '.--'  => 'W',   '-..-' => 'X',
-    '-.--' => 'Y',   '--..' => 'Z'
-  }
-  
-  morse_to_char[morse_code]
+class MorseCodeDecode
+  # defined a const MORSE_CODE hash
+
+  MORSE_CODE = {
+    '.-' => 'A',
+    '-...' => 'B',
+    '-.-.' => 'C',
+    '-..' => 'D',
+    '.' => 'E',
+    '..-.' => 'F',
+    '--.' => 'G',
+    '....' => 'H',
+    '..' => 'I',
+    '.---' => 'J',
+    '-.-' => 'K',
+    '.-..' => 'L',
+    '--' => 'M',
+    '-.' => 'N',
+    '---' => 'O',
+    '.--.' => 'P',
+    '--.-' => 'Q',
+    '.-.' => 'R',
+    '...' => 'S',
+    '-' => 'T',
+    '..-' => 'U',
+    '...-' => 'V',
+    '.--' => 'W',
+    '-..-' => 'X',
+    '-.--' => 'Y',
+    '--..' => 'Z'
+  }.freeze
+
+  # Method for character decoding of Morse code
+  def self.decode_char(code)
+    MORSE_CODE[code]
+  end
+
+  # Method for word decoding of Morse code
+  def self.decode_word(morse_word)
+    characters = morse_word.split
+    characters.map { |morse_char| decode_char(morse_char) }.join
+  end
+
+  # Method for message decoding of Morse code
+  def self.decode_message(message)
+    message.split('   ').map { |morse_word| decode_word(morse_word) }.join(' ')
+  end
 end
 
-# Method to Decode an Entire Word in Morse Code:
+char = MorseCodeDecode.decode_char('.-')
+puts char
 
-def decode_word(morse_word)
-  characters = morse_word.split(' ')
-  decoded_word = characters.map { |morse_char| decode_char(morse_char) }.join('')
-  decoded_word
-end
+word = MorseCodeDecode.decode_word('-- -.--')
+puts word
+
+message1 = MorseCodeDecode.decode_message('-- -.--   -. .- -- .')
+puts message1
+
+message2 = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
+decoded_message2 = MorseCodeDecode.decode_message(message2)
+puts decoded_message2
