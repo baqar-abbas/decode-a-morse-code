273f1ff169642d53c11e0f8e31c5952b8ac727ae
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-07-19 18:29:01 +0100

    add decode_char and decode_word method

--- decode.rb
+++ decode.rb
@@ -1,1 +1,22 @@
-puts 'Hello World'
+# Method to Decode a Morse Code Character:
+def decode_char(morse_code)
+  morse_to_char = {
+    '.-'   => 'A',   '-...' => 'B',   '-.-.' => 'C',   '-..'  => 'D',
+    '.'    => 'E',   '..-.' => 'F',   '--.'  => 'G',   '....' => 'H',
+    '..'   => 'I',   '.---' => 'J',   '-.-'  => 'K',   '.-..' => 'L',
+    '--'   => 'M',   '-.'   => 'N',   '---'  => 'O',   '.--.' => 'P',
+    '--.-' => 'Q',   '.-.'  => 'R',   '...'  => 'S',   '-'    => 'T',
+    '..-'  => 'U',   '...-' => 'V',   '.--'  => 'W',   '-..-' => 'X',
+    '-.--' => 'Y',   '--..' => 'Z'
+  }
+  
+  morse_to_char[morse_code]
+end
+
+# Method to Decode an Entire Word in Morse Code:
+
+def decode_word(morse_word)
+  characters = morse_word.split(' ')
+  decoded_word = characters.map { |morse_char| decode_char(morse_char) }.join('')
+  decoded_word
+end
